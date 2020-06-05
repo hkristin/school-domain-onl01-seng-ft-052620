f560ed861dd2e69990b5e585b1512e387177f683
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-05 19:14:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+attr_accessor :name, :roster
+  
+def initialize(name)
+    @name = name
+    @roster = {}
+end
+  
+def add_student(name,grade)
+  
+end
+
+def grade(grade)
+  roster[grade]
+end
+
+def sort
+end
+
+end
